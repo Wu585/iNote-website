9b6122448441f2fd47f3891e3318a12d51cc4bc7
Mike <15151851516@163.com>
2021-06-09 15:07:44 +0800

    deploy

---
+++ precache-manifest.24b510e245fdc652f5e6d92c762b7890.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0a11978cff7952dfe0",
+    "url": "/iNote-website/css/app.bfe06a86.css"
+  },
+  {
+    "revision": "d44cd462773df15088cf",
+    "url": "/iNote-website/css/chunk-vendors.caacd1e0.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/iNote-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/iNote-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "136ebc21d66b6daacd109fb5ecb37c96",
+    "url": "/iNote-website/img/inote.136ebc21.png"
+  },
+  {
+    "revision": "136ebc21d66b6daacd109fb5ecb37c96",
+    "url": "/iNote-website/img/inote.png"
+  },
+  {
+    "revision": "02953aae0f04eb2b25d6d899a8e8abb5",
+    "url": "/iNote-website/index.html"
+  },
+  {
+    "revision": "2d0a11978cff7952dfe0",
+    "url": "/iNote-website/js/app.b608166b.js"
+  },
+  {
+    "revision": "d44cd462773df15088cf",
+    "url": "/iNote-website/js/chunk-vendors.101ffb7c.js"
+  },
+  {
+    "revision": "4964b293e51d366aadc650ab5a7e7518",
+    "url": "/iNote-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/iNote-website/robots.txt"
+  }
+]);
